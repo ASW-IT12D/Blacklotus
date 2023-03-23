1b5482ecf71a3aaa3cd8faf0621ccc6af7f9f4ea
marc <marc.castro.chavez@estudiantat.upc.edu>
2023-03-23 13:54:07 +0100

    database issue created

--- blacklotus/models.py
+++ blacklotus/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Issue(models.Model):
+    subject = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.subject + ' ' + self.description
+
+class User(models.Model):
+    username = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=100)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.username
--- blacklotus/urls.py
+++ blacklotus/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 #URLConf
 urlpatterns = [
-    path('newissue/', views.CreateIssue),
-    path('', views.CreateIssue)
+    path('newissue/', views.CreateIssueForm),
+    path('', views.showIssues),
+    path('newissue/newissue/new/', views.CreateIssue)
 ]
