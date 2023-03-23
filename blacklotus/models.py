59e226def997ffdd32ddd3d9084f069c23715c8b
marc <marc.castro.chavez@estudiantat.upc.edu>
2023-03-23 13:54:07 +0100

    database issue created

--- blacklotus/models.py
+++ blacklotus/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Issue(models.Model):
+    subject = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
