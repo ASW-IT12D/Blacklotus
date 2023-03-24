8abbe150d96514ae6da85f9ba980b46b50d9e25b
marc <marc.castro.chavez@estudiantat.upc.edu>
2023-03-24 10:57:48 +0100

    url lista

--- blacklotus/urls.py
+++ blacklotus/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 #URLConf
 urlpatterns = [
-    path('newissue/', views.CreateIssueForm),
+    path('newissue/', views.CreateIssueForm, name="newIssue"),
     path('', views.showIssues),
     path('newissue/new/', views.CreateIssue)
 ]
