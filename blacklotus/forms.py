31591811d5be3d2bc3cf92946ee5d2bb17d15f51
Marc <marc.castro.chavez@estudiantat.upc.edu>
2023-04-03 19:14:37 +0200

    optimizacion de añadir cosas a la bd

---
+++ blacklotus/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Issue
+
+
+class IssueForm(forms.ModelForm):
+    class Meta:
+        model = Issue
+        fields = ['subject', 'status', 'description', 'type', 'severity', 'priority']
--- blacklotus/views.py
+++ blacklotus/views.py
@@ -1,7 +1,7 @@
 
 
 from django.shortcuts import render, redirect
-
+from .forms import IssueForm
 from .models import Issue
 from django.http import HttpResponse
 # Create your views here.
@@ -10,14 +10,9 @@ def CreateIssueForm(request):
     return render(request, 'newissue.html')
 
 def CreateIssue(request):
-    sub = request.POST.get("subject")
-    des = request.POST.get("description")
-    type = request.POST.get("type")
-    severity = request.POST.get("severity")
-    priority = request.POST.get("priority")
-    status = request.POST.get("status")
-    i = Issue(subject=sub, description=des, creator="Llus", status=status, type=type, severity=severity, priority=priority)
-    i.save()
+    form = IssueForm(request.POST or None)
+    if form.is_valid():
+        form.save()
     return redirect(showIssues)
 def showIssues(request):
     qs = Issue.objects.all().order_by('-creationdate')
